766deb22e8fe1a284d62a073678e991cef4599f9
Tomasz Pałucki <tpalucki@gmail.com>
2021-07-18 18:48:13 +0200

    Changes: - Downloading emails from email inbox package + parsing new email documents.

--- src/test/java/io/github/tpalucki/bbpromo/gmail/controller/GmailControllerTest.java
+++ src/test/java/io/github/tpalucki/bbpromo/gmail/controller/GmailControllerTest.java
@@ -1,5 +1,10 @@
 package io.github.tpalucki.bbpromo.gmail.controller;
 
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.junit.jupiter.api.Test;
 
 import javax.mail.*;
@@ -8,42 +13,17 @@ import java.io.IOException;
 import java.util.Enumeration;
 import java.util.Properties;
 
-
+@Slf4j
 class GmailControllerTest {
-
-
-//    ##### Mail Config #####
-//    spring.mail.host=smtp.gmail.com
-//    spring.mail.port=587
-//    spring.mail.username=tpalucki@gmail.com
-//    spring.mail.password=<login password to smtp server>
-//    spring.mail.properties.mail.smtp.auth=true
-//    spring.mail.properties.mail.smtp.starttls.enable=true
-//    spring.mail.properties.mail.smtp.starttls.required=true
-//    spring.mail.properties.mail.smtp.connectiontimeout=5000
-//    spring.mail.properties.mail.smtp.timeout=5000
-//    spring.mail.properties.mail.smtp.writetimeout=5000
 
     @Test
     void downloaFromGmail() throws IOException {
-        String host = "smtp.gmail.com";
-        String port = "587";
         String userName = "tpalucki@gmail.com";
         String password = "";
 
-        String keyword = "obserwowanym";
+        String keyword = "allegro";
 
         Properties properties = new Properties();
-
-        // server setting
-        properties.put("mail.smtp.host", host);
-        properties.put("mail.smtp.port", port);
-        properties.put("mail.smtp.user", userName);
-        properties.put("mail.smtp.auth", true);
-        properties.put("mail.smtp.starttls.enable", true);
-        properties.put("mail.smtp.starttls.required", true);
-        properties.put("mail.smtp.connectiontimeout", 5000);
-        properties.put("mail.smtp.timeout", 5000);
 
         int imapPort = 993;
         properties.put("mail.imap.host", "imap.gmail.com");
@@ -77,15 +57,11 @@ class GmailControllerTest {
                 @Override
                 public boolean match(Message message) {
                     try {
-                        for (Address from: message.getFrom()) {
-                            if (from.toString().contains("allegro")) {
+                        for (Address from : message.getFrom()) {
+                            if (from.toString().contains(keyword)) {
                                 return true;
                             }
                         }
-
-//                        if (message.getSubject().contains(keyword)) {
-//                            return true;
-//                        }
                     } catch (MessagingException ex) {
                         ex.printStackTrace();
                     }
@@ -96,9 +72,9 @@ class GmailControllerTest {
             // performs search through the folder
             Message[] foundMessages = folderInbox.search(searchCondition);
 
-            for (int i = 0; i < foundMessages.length; i++) {
-                Message message = foundMessages[i];
+            for (Message message : foundMessages) {
                 printMessage(message);
+                messageProcessing(message);
             }
 
             // disconnect
@@ -113,6 +89,15 @@ class GmailControllerTest {
         }
     }
 
+    @SneakyThrows
+    private void messageProcessing(Message message) {
+        final Document document = Jsoup.parseBodyFragment(message.getContent().toString());
+        final Element body = document.body();
+
+        log.info("Title: {}", document.title());
+        // TODO document parse using JSOUP - you can retrieve the price/new set elements
+    }
+
     private void printMessage(Message message) throws MessagingException, IOException {
         final Address[] from = message.getFrom();
         final String subject = message.getSubject();
@@ -121,10 +106,14 @@ class GmailControllerTest {
         final Flags flags = message.getFlags();
         final Address[] allRecipients = message.getAllRecipients();
         final String content = message.getContent().toString();
-        System.out.println("---------------------------------");
+        System.out.println("-----------------------------------------------------------------------------------------");
         System.out.println("Subject: " + subject);
         System.out.println("From: " + from[0]);
-        System.out.println("Text: " + content);
+//        System.out.println("Folder: " + folder);
+//        System.out.println("allHeaders = " + allHeaders);
+//        System.out.println("flags = " + flags);
+//        System.out.println("allRecipients = " + Arrays.toString(allRecipients));
+        System.out.println("Content: " + content);
     }
 
 }
